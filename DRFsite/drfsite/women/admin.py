e3d648349d853b45b42d9cd19bc011563abaf63a
Stamkovich <tamkovich1072@gmail.com>
2022-10-07 15:04:45 +0300

    'add'

--- DRFsite/drfsite/women/admin.py
+++ DRFsite/drfsite/women/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Women, Category
 
-# Register your models here.
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
